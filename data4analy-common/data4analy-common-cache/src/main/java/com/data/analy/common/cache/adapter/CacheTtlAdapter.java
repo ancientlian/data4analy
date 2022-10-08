3f3a405e7a64f3d6e4d938b71872bf9048ec0bb3
Ancientlian <940262336@qq.com>
2022-10-08 22:22:10 +0800

    添加缓存公共模块

---
+++ data4analy-common/data4analy-common-cache/src/main/java/com/data/analy/common/cache/adapter/CacheTtlAdapter.java
@@ -0,0 +1,19 @@
+package com.data.analy.common.cache.adapter;
+
+import com.data.analy.common.cache.bo.CacheNameWithTtlBO;
+
+import java.util.List;
+
+/**
+ * 实现该接口之后，根据缓存的cacheName和ttl将缓存进行过期
+ *
+ */
+public interface CacheTtlAdapter {
+
+	/**
+	 * 根据缓存的cacheName和ttl将缓存进行过期
+	 * @return 需要独立设置过期时间的缓存列表
+	 */
+	List<CacheNameWithTtlBO> listCacheNameWithTtl();
+
+}
---
+++ data4analy-common/data4analy-common-cache/src/main/java/com/data/analy/common/cache/constant/CacheNames.java
@@ -0,0 +1,21 @@
+package com.data.analy.common.cache.constant;
+
+/**
+ * 缓存名字
+ *
+
+ */
+public interface CacheNames extends RbacCacheNames, UserCacheNames {
+    /**
+     *
+     * 参考CacheKeyPrefix
+     * cacheNames 与 key 之间的默认连接字符
+     */
+    String UNION = "::";
+
+    /**
+     * key内部的连接字符（自定义）
+     */
+    String UNION_KEY = ":";
+
+}
